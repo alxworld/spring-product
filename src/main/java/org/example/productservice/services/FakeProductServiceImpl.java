71449441328d90438dc111693740034795adba5a
Alex <jsalex@gmail.com>
2024-03-05 00:54:04 +0530

    Fake store api - on Fake service impl

--- src/main/java/org/example/productservice/services/FakeProductServiceImpl.java
+++ src/main/java/org/example/productservice/services/FakeProductServiceImpl.java
@@ -1,14 +1,28 @@
 package org.example.productservice.services;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service("FakeProductService")
 public class FakeProductServiceImpl  implements  ProductService {
+
+    private RestTemplateBuilder restTemplateBuilder;
+
+    private final String PRODUCT_URL = "https://fakestoreapi.com/products/1";
+
+    public FakeProductServiceImpl(RestTemplateBuilder restTemplateBuilder){
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
     @Override
     public String getProductById(Long id) {
-        return "On Fake Prod Service => Fetching product with id " + id;
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(PRODUCT_URL, String.class);
+        return "On Fake Prod Service => Fetching product with id " + responseEntity.toString();
     }
 
     @Override
